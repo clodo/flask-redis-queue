8f877f2bf27fc6e9487a649ffe1465667372c152
Clodo <claudiobidau@gmail.com>
2017-04-20 17:26:27 -0300

    Add postgresql and SQLalchemy requirements and config parameter for database

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = 'la la la la, life is life'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 
 class ProductionConfig(Config):
